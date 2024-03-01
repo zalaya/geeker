bb65795dad8734c658fee6d40b97c9ca59ccf49c
zalaya <enol.corzal@gmail.com>
2024-03-01 19:34:30 +0100

    feat: add the github api fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
+import { useState } from "react"
 import UserCard from "./components/UserCard"
+import SearchBar from "./components/SearchBar"
 
 const App = () => {
-  const user = {
-    name: "Buenas",
-    description: "Prueba",
-    bio: "Estoes una descipr de prueba",
-    login: "buenas"
+  const [username, setUsername] = useState("")
+  const [user, setUser] = useState(null)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    fetch(`https://api.github.com/users/${username}`)
+      .then(response => response.json())
+      .then(data => setUser(data))
+  }
+
+  const handleChange = (event) => {
+    setUsername(event.target.value)
   }
 
   return (
-    <div
-      className="min-h-screen bg-gray-900"  
-    >
-      <UserCard
-        user={user}
+    <div className="min-h-screen flex flex-col items-center justify-center gap-5 bg-gray-900">
+      <SearchBar 
+        placeholder="GitHub username..."
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
       />
+      {user && user.message !== "Not Found" && (
+        <UserCard user={user} />
+      )}
     </div>
-    
-  )
+  ) 
 }
 
 export default App
